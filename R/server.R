ab1f4d4f9c498e73075563ec14215c8394680488
katiecoburn <katie.m.coburn@gmail.com>
2014-11-23 23:49:52 -0800

    Update server.R

--- R/server.R
+++ R/server.R
@@ -1,9 +1,4 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
 
 library(shiny)
 library(shinyAce)
